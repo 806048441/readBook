693ff65217e399c1dbcabbb6532d48870726cf6f
806048441 <806048441@qq.com>
2019-11-06 19:30:52 +0800

    神州第一坑
    描述

--- 《C语言程序设计（从入门到精通）》何亦琛、古万荣/第八章.常见算法/8.2常见算法/8.2常见算法/llMain.c
+++ 《C语言程序设计（从入门到精通）》何亦琛、古万荣/第八章.常见算法/8.2常见算法/8.2常见算法/llMain.c
@@ -16,19 +16,82 @@ void zheban(int *from,int number,int x);
 
 void charu(int *from,int number);
 
+void zuidazhi(int *from,int number);
+
 void llMain(void){
     
-    int number=10;
-    int from[number];
-    
-    srand((unsigned)time(NULL));
-    for (int i =0; i<number; i++) {
+//    int count=0;
+//    int temp[count];
+//    for (int i=0; i<6; i++) {
+//
+//        count =i+1;
+//        int array[count];
+//
+//        for (int j=0; j<count; j++) {
+//
+//            if (j==0 || j==i) {
+//
+//                array[j]=1;
+//            }else{
+//
+//                array[j]=temp[j]+temp[j-1];
+//            }
+//
+//        }
+//
+//        for (int n=0; n<count; n++) {
+//
+//            temp[n]=array[n];
+//
+//            printf("%d ",temp[n]);
+//        }
+//
+//        putchar('\n');
+//
+//    }
+
+    int i,j,n=0,a[17][17]={0};
         
-        from[i]=rand()%1000+100;
-    }
+        while(n<1 || n>16)
+            
+        { printf("请输入杨辉三角形的行数:");
+            
+            scanf("%d",&n);
+            
+        }
+        
+        for(i=0;i<n;i++)
+            
+            a[i][0]=1; /*第一列全置为一*/
+        
+        for(i=1;i<n;i++)
+            
+            for(j=1;j<=i;j++)
+                
+                a[i][j]=a[i-1][j-1]+a[i-1][j];/*每个数是上面两数之和*/
+        
+        for(i=0;i<n;i++) /*输出杨辉三角*/
+            
+        { for(j=0;j<=i;j++)
+            
+            printf("%5d",a[i][j]);
+            
+            printf("\n");
+            
+        }
     
-//    maopao(from, number);
-    charu(from, number);
+//    int number=10;
+//    int from[number];
+//    
+//    srand((unsigned)time(NULL));
+//    for (int i =0; i<number; i++) {
+//        
+//        from[i]=rand()%1000+100;
+//    }
+//    
+////    maopao(from, number);
+////    charu(from, number);
+//    zuidazhi(from, number);
 }
 
 void maopao(int *from,int number){
@@ -93,24 +156,44 @@ void zheban(int *from,int number,int x){
 void charu(int *from,int number){
     
     int t ,j;
+    
     for (int i=1; i<number; i++) {
         
         t =from[i];
-        j=i-1;
-        
-        while (j>=0 && t<from[i]) {
+        j= i-1;
+        while (j>=0 && t<from[j]) {
             
             from[j+1]=from[j];
             j--;
         }
+        from[j+1]=t;
         
-        printf("第%d次排序结果:",i+1);
-        for (int k=0; k<number; k++) {
+        for (int n=0; n<number; n++) {
             
-            printf("%d--",from[k]);
+            printf("%d-",from[n]);
         }
         putchar('\n');
     }
+}
+
+void zuidazhi(int *from,int number){
+    
+    int a = from[0];
+    
+    for (int i=1; i<number; i++) {
+        
+        if (from[i]>a) {
+            a= from[i];
+        }
+    }
+    
+    for (int i=0; i<number; i++) {
+        
+        printf("%d-",from[i]);
+    }
+    putchar('\n');
+    
+    printf("最大值%d\n",a);
 }
 
 
